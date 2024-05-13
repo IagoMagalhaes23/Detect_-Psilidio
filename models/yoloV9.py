b9a32823aca0bc5b3f425f1db273c4d9cb9afa19
IagoMagalhaes23 <iagomagalhaes23@gmail.com>
2024-05-13 15:26:12 -0300

    create class yolov9.py

---
+++ models/yoloV9.py
@@ -0,0 +1,6 @@
+'''
+    Yolo V9
+    Autores: Iago Magalhães, Larissa Ferreira e Iális Calvalcante
+    Data: 13/05/2024
+    Descrição: Classe para treinamento do modelo customizado do Yolo V9.
+'''
